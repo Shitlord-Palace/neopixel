5b82b1f987197ab5fa5d125809971bf71ef9d143
little-scale <seb.tomczak@gmail.com>
2015-08-18 20:50:07 +0930

    Pac
    Mang

---
+++ pacmang/pacmang.ino
@@ -0,0 +1,174 @@
+#include <Adafruit_NeoPixel.h>
+#include <avr/power.h>
+#define PIN            6
+#define NUMPIXELS      24
+Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
+
+// Game settings
+int SPEED_INCREMENT = 5;
+int GREEN_FOOD = 50;
+int BLUE_FOOD = 150;
+int RED_FOOD = 200;
+int SPAWN_RATE = 6; // Spawn every 1/SPAWN_RATE ticks.
+
+// Power up active
+boolean _powered = false;
+// Current speed
+int _speed = 25;
+int _direction = 1;
+int _location = 0;
+// Color
+int _r = 1;
+int _g = 1;
+int _b = 1;
+
+// State values
+int _state[24];
+const int EMPTY = 0;
+const int PELLET = 1;
+const int GHOST = 2;
+const int POWER = 3;
+
+int _lastPress = 1;
+
+void setup() 
+{
+  pixels.begin();
+  pinMode(3, INPUT);
+  digitalWrite(3, HIGH);  
+}
+
+void loop()
+{
+
+  
+    if (_r == 255 && _b == 255 && _g == 255) 
+    {
+      endState();
+    }
+    else
+    {
+      play();
+    }
+}
+
+void play()
+{
+    
+    int buttonPress = digitalRead(3);
+    int lastPress = buttonPress;
+    buttonPress = buttonPress ^ _lastPress;
+    _lastPress = lastPress;
+    if (buttonPress != 0 && lastPress == 0) _direction -= 2 * _direction; 
+    
+    //clean()
+    spawn();
+ 
+    int pac = _location;
+    int state = _state[_location];
+    
+    // Eat a pellet
+    if (state == PELLET)
+    {
+      _g = constrain (_g + GREEN_FOOD, 0, 255);
+      _speed += SPEED_INCREMENT;    
+    }
+    // Hit a ghost
+    else if (state == GHOST)
+    {
+      if (_powered)
+      {
+        _b = constrain(_b + BLUE_FOOD, 0, 255);
+        //TODO
+        _powered = false;
+        _r = false;
+      }
+      else
+      {
+        _g = 1;
+        _b = 1;
+      }
+    } 
+    // Eat a power-up
+    else if (state == POWER)
+    {
+      _powered = true;
+      _r = constrain(_r + RED_FOOD, 0, 255);
+    }
+
+    // Set pacman colour
+    if (_powered) 
+    {
+      pixels.setPixelColor(pac, pixels.Color(255, 0, 0));
+    }
+    else
+    {
+      pixels.setPixelColor(pac, pixels.Color(_r, _g, _b));
+    }
+    pixels.show();
+    delay(100);
+    
+    pixels.setPixelColor(pac, pixels.Color(0, 0, 0));
+    _state[_location] = EMPTY;
+  
+    move();
+}
+
+void move()
+{
+  if (_direction < 0 && _location == 0) _location = 23;
+  else if (_direction > 0 && _location == 23) _location = 0;
+  else _location += _direction;
+}
+
+void spawn()
+{
+  boolean doSpawn = random(SPAWN_RATE) == 1;
+  if (!doSpawn) return;
+  
+  int spawnPoint = random(24);
+  
+  // Decide type by rolling a d20.
+  int d20roll = random(20) + 1;
+  int type;
+  if (d20roll <= 2) type = GHOST;
+  else if (d20roll == 20) type = POWER;
+  else type = PELLET;
+  
+  // Add new spawned thing
+  _state[spawnPoint] = type;
+  
+  // Draw that shit
+  switch(type)
+  {
+    case PELLET:
+      pixels.setPixelColor(spawnPoint, pixels.Color(0, 100, 0));
+      break;
+    case GHOST:
+      pixels.setPixelColor(spawnPoint, pixels.Color(0, 0, 100));
+      break;
+    case POWER:
+      pixels.setPixelColor(spawnPoint, pixels.Color(255, 255, 255));
+      break;
+    case EMPTY:
+      pixels.setPixelColor(spawnPoint, pixels.Color(0, 0, 0));
+      break;
+    }
+    
+    pixels.show();
+}
+
+
+void endState()
+{
+  for (int i = 0; i < 24; i++)
+  {
+    if (i == random(3)) pixels.setPixelColor(i, pixels.Color(255, 255, 255));
+    else pixels.setPixelColor(i, pixels.Color(0, 0, 0));
+    
+    pixels.show();
+    delay(666);
+  }
+}
+
+
